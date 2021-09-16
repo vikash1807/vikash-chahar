bfbaa5efbc78ca6483fa866702edcd08839f5cbe
vikash1807 <86599432+vikash1807@users.noreply.github.com>
2021-09-16 20:11:51 +0530

    Add files via upload
    product of two numbers

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x,y;
+    cout << "enter value of 1st number : ";
+    cin >> x;
+    cout << "enter value of 2nd number : ";
+    cin >> y;
+    cout << "value of product of both numbers : " << x*y;
+    return 0;
+}
